a930e50d2afce19349ea52f6e901af1d28bd0eb9
github scarlettutopia <github scarlettutopia@aol.com>
2019-09-12 14:29:53 -0700

    added seed data and updated methods

--- bin/Run_Insectarium.rb
+++ bin/Run_Insectarium.rb
@@ -2,50 +2,58 @@ require_relative '../config/environment'
 class Insectarium
 
     def self.run
-        puts "Welcom To The Insectarium!  What is your name?"
-        username = STDIN.gets.chomp
-
-        if User.exists?(username) == true
-            puts "Welcome back #{username}!"
-            self.second_menu
-        else
-            self.main_menu
-        end
-
         is_running = true
         while is_running
-            self.main_menu
+            puts "Welcom To The Insectarium!  What is your name?"
+            username = STDIN.gets.chomp
+
+            # puts 'Checking to see if Megan account exists'
+        # User.exists?(username: username)
+           if found = User.exists?(username: username)
+                # puts "found? #{found}"
+                puts "Welcome back #{username}!"
+                self.insect_menu
+           elsif found == false
+                puts "That username does not currently exist"
+                is_running = true
+           end
+
+        # is_running = true
+        # while is_running
+            self.profile_menu
+            is_running = true
             option = STDIN.gets.chomp
             if option == "1"
                 self.create_new_user
-            elsif option == "2" || option == self.second_menu
+            elsif option == "2" || option == self.insect_menu
             elsif option == "3" 
                 is_running = false
             end
 
-            self.second_menu
+            self.insect_menu
             option = STDIN.gets.chomp
-            if option is == "1"
+            if option == "1"
                 self.add_new_insect
-            elsif option is == "2"
+            elsif option == "2"
                 self.identify_insect
-            elsif option is == "3"
+                
+            elsif option == "3"
                 is_running = false
             end
         end
     end
 
 
-    def self.main_menu
+    def self.profile_menu
         puts "Would you like to create a profile or continue as a guest?"
         puts "1. Create New User Profile"
         puts "2. Continue As Guest User"
         puts "3. Exit"
     end
 
-    def self.second_menu
+    def self.insect_menu
         puts "What would you like to do?"
-        puts "1. Add an insect to the Insectatium" 
+        puts "1. Add an insect to the Insectarium" 
         puts "2. Identify an insect"
         puts "3. Exit"
     end
@@ -54,14 +62,20 @@ class Insectarium
         puts "Creating a new account. What's your username?"
         username = STDIN.gets.chomp
         User.create(username: username)
+        puts "Creating account for #{username}"
     end
 
-    def self.add_new_insect(leg_count, main_color, accent_color, name)
-        if Insetcs.exists?(name) == true
-            puts "This insect already exists in the Insectarium.  
-            Please find another crawly critter and try again."
-        else
-        end
+    def self.add_new_insect
+        puts "Please enter the following insect information... name, main color, accent color, leg count"
+        name, main_color, accent_color, leg_count = STDIN.gets.chomp.split(",")
+        if found = Insect.exists?(name: name)
+            puts "That insect already exists in the Insectarium.  Please find another crawly critter and try again."
+            self.insect_menu
+       else
+            Insect.create(name: name, main_color: main_color, accent_color: accent_color, leg_count: leg_count)
+            puts "We've added a new #{name} to the Insectarium!  NEXT..."
+            self.insect_menu
+       end
     end
 
     def self.identify_insect(leg_count, main_color, accent_color)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,60 @@
-User.destroy_all
-Insect.destroy_all
+# User.destroy_all
+# Insect.destroy_all
 
-puts "Creating account for Megan"
-User.create(username: "Megan")
-puts 'Checking to see if Megan account exists"'
-found = User.exists?("Megan")
-puts "found? #{found}"
+# puts "Creating account for Megan"
+# u1 = User.create(username: "Megan")
+# puts 'Checking to see if Megan account exists"'
+# found = User.exists?("Megan")
+# puts "found? #{found}"
 
-puts 'Checking to see if Megan account exists using find_by"'
-found = User.find_by(username: "Megan")
-puts "found? #{found}"
+# puts 'Checking to see if Megan account exists using find_by"'
+# found = User.find_by(username: "Megan")
+# puts "found? #{found}"
+
+u2 = User.create(username: "Lisa")
+u3 = User.create(username: "Tammy")
+u4 = User.create(username: "Mark")
+u5 = User.create(username: "Kevin")
+u6 = User.create(username: "Jen")
+u7 = User.create(username: "Sabrina")
+u8 = User.create(username: "Katie")
+u9 = User.create(username: "Hayley")
+u10 = User.create(username: "Bonnie")
+
+
+
+
+
+
+
+
+bee1 = Insect.create(name: "American Bumble Bee", main_color: "Black", accent_color: "Yellow", leg_count: 6)
+bee2 = Insect.create(name: "Bald-faced Hornet", main_color: "Black", accent_color: "Yellow", leg_count: 6)
+bee3 = Insect.create(name: "Golden Northern Bumble Bee", main_color: "Black", accent_color: "Yello", leg_count: 6)
+bee4 = Insect.create(name: "Honey Bee", main_color: "Black", accent_color: "Yellow", leg_count: 6)
+wasp1 = Insect.create(name: "Potter Wasp", main_color: "Black", accent_color: "Yellow", leg_count: 6)
+wasp2 = Insect.create(name: "Squarehead Wasp", main_color: "Black", accent_color: "Yellow", leg_count: 6)
+wasp3 = Insect.create(name: "Weevil Wasp", main_color: "Black", accent_color: "Yellow", leg_count: 6)
+wasp4 = Insect.create(name: "Yellow Jacket", main_color: "Black", accent_color: "Yellow", leg_count: 6)
+spider1 = Insect.create(name: "Arabesque Orbweaver", main_color: "Black", accent_color: "White", leg_count: 8)
+spider2 = Insect.create(name: "Bold Jumper", main_color: "Black", accent_color: "White", leg_count: 8)
+spider3 = Insect.create(name: "Jumping Spider", main_color: "Black", accent_color: "White", leg_count: 8)
+spider4 = Insect.create(name: "North American Jumping Spider", main_color: "Black", accent_color: "White", leg_count: 8)
+spider5 = Insect.create(name: "Shamrock Spider", main_color: "Black", accent_color: "White", leg_count: 8)
+spider6 = Insect.create(name: "Spinybacked Orbweaver", main_color: "Black", accent_color: "Yellow", leg_count: 8)
+spider7 = Insect.create(name: "Black and Yellow Garden Spider", main_color: "Black", accent_color: "Yellow", leg_count: 8)
+spider8 = Insect.create(name: "Venusta Orchard Spider", main_color: "Black", accent_color: "Green", leg_count: 8)
+spider9 = Insect.create(name: "Black Widow", main_color: "Black", accent_color: "Red", leg_count: 8)
+spider10 = Insect.create(name: "Red-spotted Ant Mimic Spider", main_color: "Black", accent_color: "Red", leg_count: 8)
+spider11 = Insect.create(name: "Western Black Widow", main_color: "Black", accent_color: "Red", leg_count: 8)
+spider12 = Insect.create(name: "Woodland Jumping Spider", main_color: "Black", accent_color: "Red", leg_count: 8)
+dragonfly1 = Insect.create(name: "Familiar Bluet", main_color: "Blue", accent_color: "Black", leg_count: 4)
+dragonfly2 = Insect.create(name: "Orange Meadowhawk", main_color: "Orange", accent_color: "Black", leg_count: 4)
+dragonfly3 = Insect.create(name: "Wandering Glider", main_color: "Orange", accent_color: "Black", leg_count: 4)
+dragonfly4 = Insect.create(name: "Blue Dasher", main_color: "Blue", accent_color: "Black", leg_count: 4)
+beetle1 = Insect.create(name: "Convergent Lady Beetle", main_color: "Red", accent_color: "Black", leg_count: 6)
+beetle2 = Insect.create(name: "Banded Alder Borer Beetle", main_color: "Black", accent_color: "White", leg_count: 6)
+beetle3 = Insect.create(name: "Scarlet Malachite Beetle", main_color: "Red", accent_color: "Black", leg_count: 6)
+beetle4 = Insect.create(name: "Mottled Tortoise Beetle", main_color: "Gold", accent_color: "Black", leg_count: 6)
+beetle5 = Insect.create(name: "Dogbane Leaf Beetle", main_color: "Green", accent_color: "Gold", leg_count: 6)
+beetle6 = Insect.create(name: "House Centipede", main_color: "Ivory", accent_color: "Black", leg_count: 30)
