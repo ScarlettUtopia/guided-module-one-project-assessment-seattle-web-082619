9c1e083cf0a6fde9fc0409e3b5729b6fae3591fd
github scarlettutopia <github scarlettutopia@aol.com>
2019-09-12 16:05:26 -0700

    Updated Menus and some methods

--- bin/Run_Insectarium.rb
+++ bin/Run_Insectarium.rb
@@ -2,33 +2,28 @@ require_relative '../config/environment'
 class Insectarium
 
     def self.run
-        is_running = true
-        while is_running
+        
             puts "Welcom To The Insectarium!  What is your name?"
             username = STDIN.gets.chomp
 
-            # puts 'Checking to see if Megan account exists'
-        # User.exists?(username: username)
            if found = User.exists?(username: username)
-                # puts "found? #{found}"
                 puts "Welcome back #{username}!"
-                self.insect_menu
+                # self.insect_menu
            elsif found == false
                 puts "That username does not currently exist"
+                self.profile_menu
                 is_running = true
+                option = STDIN.gets.chomp
+                if option == "1"
+                    self.create_new_user
+                elsif option == "2" || option == self.insect_menu
+                elsif option == "3" 
+                    is_running = false
+                end
            end
 
-        # is_running = true
-        # while is_running
-            self.profile_menu
-            is_running = true
-            option = STDIN.gets.chomp
-            if option == "1"
-                self.create_new_user
-            elsif option == "2" || option == self.insect_menu
-            elsif option == "3" 
-                is_running = false
-            end
+        is_running = true
+        while is_running
 
             self.insect_menu
             option = STDIN.gets.chomp
@@ -36,7 +31,6 @@ class Insectarium
                 self.add_new_insect
             elsif option == "2"
                 self.identify_insect
-                
             elsif option == "3"
                 is_running = false
             end
@@ -51,12 +45,14 @@ class Insectarium
         puts "3. Exit"
     end
 
+
     def self.insect_menu
         puts "What would you like to do?"
         puts "1. Add an insect to the Insectarium" 
         puts "2. Identify an insect"
         puts "3. Exit"
     end
+
 
     def self.create_new_user
         puts "Creating a new account. What's your username?"
@@ -65,21 +61,45 @@ class Insectarium
         puts "Creating account for #{username}"
     end
 
+
     def self.add_new_insect
-        puts "Please enter the following insect information... name, main color, accent color, leg count"
+        puts "Please enter the following insect information... 
+        name, main color, accent color, leg count"
         name, main_color, accent_color, leg_count = STDIN.gets.chomp.split(",")
         if found = Insect.exists?(name: name)
-            puts "That insect already exists in the Insectarium.  Please find another crawly critter and try again."
-            self.insect_menu
+            puts "That insect already exists in the Insectarium.
+            Please find another crawly critter and try again. :)"
+            # self.insect_menu
        else
             Insect.create(name: name, main_color: main_color, accent_color: accent_color, leg_count: leg_count)
-            puts "We've added a new #{name} to the Insectarium!  NEXT..."
-            self.insect_menu
+            puts "We've added a new #{name} to the Insectarium!
+            Thank you for your contribution to our crawly critters collection!"
+            # self.insect_menu
        end
     end
 
-    def self.identify_insect(leg_count, main_color, accent_color)
+
+    def self.identify_insect
+        puts "Please enter the following identifiers...
+        main color, accent color, leg count"
+        main_color, accent_color, leg_count = STDIN.gets.chomp.split(",")
+        if found = Insect.exists?(main_color: main_color, accent_color: accent_color, leg_count: leg_count)
+        
+        else 
+            puts "This insect does not yet exist in Insectarium.
+            You may have found an alien! 8P
+            Would you like to add this creature to the Insectarium?
+            1. Yes
+            2. No"
+        end
+
+        if option == "1"
+            self.add_new_insect
+        elsif option == "2"
+            self.insect_menu
+        end
     end
+
 
 end
 
